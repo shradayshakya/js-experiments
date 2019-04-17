3c45affce9d8ae8d1bf12f53bf2f39db5a5d6c1e
Shraday Shakya <shradayshakya@gmail.com>
2019-04-17 21:34:33 +0545

    Refactor automatic transition

--- ast1/script.js
+++ ast1/script.js
@@ -1,57 +1,64 @@
+//Constants declaration and assignment
+var IMAGE_WIDTH = 960;
+var LEFT_TO_RIGHT_SPEED = -10; //Less is more
+var RIGHT_TO_LEFT_SPEED = 50;
+var PAUSE_DURATION = 500; //In milliseconds
 
-var wrapper = document.getElementById("carousel-images");
+//Variables declaration and assignment
+var wrapper = document.getElementById("carousel-wrapper");
+var totalWrapperWidth = wrapper.childElementCount * IMAGE_WIDTH;
+var minLeft = -(totalWrapperWidth - IMAGE_WIDTH);
+wrapper.style.width = totalWrapperWidth + "px";
+var leftPosition = 0;
+var offset;
+var isLeftToRight;
+var requestId;
 
-// var leftButton = document.createElement("img");
-// leftButton.setAttribute("src" ,"images/left.png");
-// leftButton.style.width = "30px";
-// leftButton.style.height = "30px";
-// leftButton = 
-// console.log(leftButton);
+// Functions for animation BEGIN
 
-
-
-var left = 0;
-var offset = 5;
-var imageWidth = 960;
-var numberOfImages = wrapper.getElementsByTagName("img").length;
-var leftToRightFlag = 1;
-
-var FPS = 60;
-var LEFT_TO_RIGHT_RATE = -5;
-var RIGHT_TO_LEFT_RATE = 50
-var MAX_WIDTH = -(imageWidth * (numberOfImages - 1));
-
-var start = function() {
-  var currentInterval = setInterval(function() {
-    slide();
-    if (left % imageWidth == 0 && leftToRightFlag== 1) {
-      pauseSlide(currentInterval);
-    }
-  }, 1000 / FPS);
-};
-
-function pauseSlide(currentInterval) {
-  clearInterval(currentInterval);
-
-  setTimeout(function() {
-    start();
-  }, 1000);
+//Starts the tranisition animation
+function startTransition() {
+  slide();
+  if (leftPosition % IMAGE_WIDTH == 0 && isLeftToRight) {
+    pauseTransition(requestId);
+  } else {
+    requestId = requestAnimationFrame(startTransition);
+  }
 }
 
+//Slides the wrapper left or right and pauses if left is zero
 function slide() {
-if(left<=MAX_WIDTH){
-    offset = RIGHT_TO_LEFT_RATE;
-    leftToRightFlag = 0;
+  if (leftPosition <= minLeft) {
+    offset = RIGHT_TO_LEFT_SPEED;
+    isLeftToRight = false;
+  } else if (leftPosition >= 0) {
+    offset = LEFT_TO_RIGHT_SPEED  ;
+    isLeftToRight = true;
+  }
+
+  if (leftPosition == 0) {
+    setTimeout(function() {
+      updateWrapperPosition();
+    }, PAUSE_DURATION);
+  } else {
+    updateWrapperPosition();
+  }
 }
 
-if(left >= 0){
-    offset = LEFT_TO_RIGHT_RATE;
-    leftToRightFlag = 1;
+//Updates wrapper position
+function updateWrapperPosition() {
+  leftPosition += offset;
+  wrapper.style.left = leftPosition + "px";
 }
 
-//   offset = (left <= MAX_WIDTH || left >= 0)? (offset *= -1): offset;
-  left += offset;
-  wrapper.style.left = left + "px";
+//pauses transition of a second
+function pauseTransition(requestId) {
+  window.cancelAnimationFrame(requestId);
+  setTimeout(function() {
+    startTransition();
+  }, PAUSE_DURATION);
 }
-start();
 
+// Functions for animation END
+
+startTransition();
