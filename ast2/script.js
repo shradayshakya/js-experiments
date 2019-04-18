ddefdc2a7c6a689b0cbd1c7fe0d3daa9221de6fe
Shraday Shakya <shradayshakya@gmail.com>
2019-04-19 05:32:40 +0545

    Assignment second collision detection

--- ast2/script.js
+++ ast2/script.js
@@ -1,186 +1,193 @@
-//Constants declaration and assignment
-var IMAGE_WIDTH = 960;
-var AUTO_TRANS_SPEED = 10;
-var EVENT_TRANS_SPEED = 50;
-var PAUSE_DURATION = 500; //In milliseconds
-
-//Variables declaration and assignment
-var wrapper = document.getElementById("carouselWrapper");
-var container = document.getElementById("container");
-var totalWrapperWidth = wrapper.childElementCount * IMAGE_WIDTH;
-var minLeft = -(totalWrapperWidth - IMAGE_WIDTH);
-wrapper.style.width = totalWrapperWidth + "px";
-var leftPosition = 0;
-var offset;
-var requestId;
-var indicatorButtons = [];
-var leftButton;
-var rightButton;
-var eventState;
-var eventTravelPosition;
- var isLeftToRight;
-
-
-// Carousel images transition BEGIN
-
-//Starts the tranisition animation
-function startTransition() {
-  slide();
-  if (leftPosition % IMAGE_WIDTH == 0 && !eventState) {
-    pauseTransition(requestId);
-  } else {
-    requestId = requestAnimationFrame(startTransition);
+class Circle {
+  constructor(ctx, x, y, xVelocity, yVelocity, radius, color) {
+    this.ctx = ctx;
+    this.x = x;
+    this.y = y;
+    this.velocity = {
+      x: xVelocity,
+      y: yVelocity
+    };
+    this.radius = radius;
+    this.color = color;
   }
-}
 
-//Slides the wrapper left or right and pauses if leftPosition is zero
-function slide() {
-  if(eventState){
-    var deviation = Math.abs(leftPosition - eventTravelPosition);
-    console.log("Left Position:"+leftPosition+"   ||||||| Event Travel Position:"+ eventTravelPosition + " ||||||| Deviation:"+deviation);
-    if(deviation < IMAGE_WIDTH){
-      eventState = false;
+  draw() {
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    this.ctx.fillStyle = this.color;
+    this.ctx.fill();
+  }
+
+  update(width, height, circles, calculateDistance) {
+    this.draw();
+    for (let i = 0; i < circles.length; i++) {
+      if (this === circles[i]) continue;
+      if (
+        calculateDistance(this.x, this.y, circles[i].x, circles[i].y) -
+          this.radius * 2 <
+        0
+      ) {
+        this.resolveCollision(this, circles[i]);
+      }
+    }
+
+    if (this.x > width - this.radius || this.x < this.radius) {
+      this.velocity.x = -this.velocity.x;
+    }
+
+    if (this.y > height - this.radius || this.y < this.radius) {
+      this.velocity.y = -this.velocity.y;
+    }
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  }
+
+  rotate(velocity, angle) {
+    const rotatedVelocities = {
+      x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+      y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+    };
+    return rotatedVelocities;
+  }
+
+  //One Dimentional Newtonian
+  resolveCollision(circleA, circleB) {
+    const xVelocityDiff = circleA.velocity.x - circleB.velocity.x;
+    const yVelocityDiff = circleA.velocity.y - circleB.velocity.y;
+
+    const xDist = circleB.x - circleA.x;
+    const yDist = circleB.y - circleA.y;
+
+    //Only if circles aren't overlapping
+    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+
+      //Angles for rotating circles such that the circles aren't moving in only in one dimention
+      const angle = -Math.atan2(circleB.y - circleA.y, circleB.x - circleA.x);
+
+      //Mass of circles
+      const m1 = 1;
+      const m2 = 1;
+
+      // Velocity before equation
+      const u1 = this.rotate(circleA.velocity, angle);
+      const u2 = this.rotate(circleB.velocity, angle);
+
+      // Velocity after 1d collision newtonian equation
+      const v1 = {
+        x: (u1.x * (m1 - m2)) / (m1 + m2) + (u2.x * 2 * m2) / (m1 + m2),
+        y: u1.y
+      };
+      const v2 = {
+        x: (u2.x * (m1 - m2)) / (m1 + m2) + (u1.x * 2 * m2) / (m1 + m2),
+        y: u2.y
+      };
+
+      // Final velocity after rotating axis back to original location
+      const vFinal1 = this.rotate(v1, -angle);
+      const vFinal2 = this.rotate(v2, -angle);
+
+      // Swap circleA velocities for realistic bounce effect
+      circleA.velocity.x = vFinal1.x;
+      circleA.velocity.y = vFinal1.y;
+
+      circleB.velocity.x = vFinal2.x;
+      circleB.velocity.y = vFinal2.y;
     }
   }
+}
 
-  if (leftPosition >= 0) {
-    offset = (eventState)? -EVENT_TRANS_SPEED: -AUTO_TRANS_SPEED;
-    isLeftToRight = false;
-  }else if (leftPosition <= minLeft) {
-    offset = (eventState)? EVENT_TRANS_SPEED: AUTO_TRANS_SPEED;
-    isLeftToRight = true;
+class Canvas {
+  constructor(canvas, height, width) {
+    this.element = canvas;
+    this.width = width;
+    this.height = height;
+    this.element.width = width;
+    this.element.height = height;
+    this.colors = [
+      "aqua",
+      "black",
+      "blue",
+      "fuchsia",
+      "gray",
+      "green",
+      "lime",
+      "maroon",
+      "navy",
+      "olive",
+      "orange",
+      "purple",
+      "red",
+      "silver",
+      "teal",
+      "yellow"
+    ];
+    this.ctx = this.element.getContext("2d");
   }
 
-  if (leftPosition == 0 && !eventState) {
-    setTimeout(function() {
-      leftPosition += offset;
-      updateWrapperPosition(leftPosition);
-    }, PAUSE_DURATION);
-  } else {
-     leftPosition += offset;
-      updateWrapperPosition(leftPosition);
+  randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  start() {
+    this.circles = [];
+    for (let i = 0; i < 50; i++) {
+      let radius = 10;
+      let x = this.randomIntFromInterval(radius * 2, this.width - radius * 2);
+      let y = this.randomIntFromInterval(radius * 2, this.width - radius * 2);
+      let xVelocity = (Math.random() - 0.5) * 10;
+      let yVelocity = (Math.random() - 0.5) * 10;
+
+      //to avoid overlapping circles
+      if (i !== 0) {
+        for (let j = 0; j < this.circles.length; j++) {
+          if (
+            this.calculateDistance(x, y, this.circles[j].x, this.circles[j].y) -
+              radius * 2 <
+            5
+          ) {
+            x = this.randomIntFromInterval(radius * 2, this.width - radius * 2);
+            y = this.randomIntFromInterval(radius * 2, this.width - radius * 2);
+            j = -1;
+          }
+        }
+      }
+
+      this.circles.push(
+        new Circle(
+          this.ctx,
+          x,
+          y,
+          xVelocity,
+          yVelocity,
+          radius,
+          this.colors[i % this.colors.length]
+        )
+      );
+    }
+
+    this.animate();
+  }
+
+  animate() {
+    this.ctx.clearRect(0, 0, innerWidth, innerHeight);
+    for (let i = 0; i < this.circles.length; i++) {
+      this.circles[i].update(
+        this.width,
+        this.height,
+        this.circles,
+        this.calculateDistance
+      );
+    }
+    requestAnimationFrame(() => this.animate());
+  }
+
+  calculateDistance(x1, y1, x2, y2) {
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+    return Math.sqrt(dx * dx + dy * dy);
   }
 }
 
-//Updates wrapper position
-function updateWrapperPosition(leftPosition) {
-  wrapper.style.left = leftPosition + "px";
-}
-
-//pauses transition of a second
-function pauseTransition(requestId) {
-  window.cancelAnimationFrame(requestId);
-  setTimeout(function() {
-    startTransition();
-  }, PAUSE_DURATION);
-}
-
-// Carousel images transition animation END
-
-//Returns an image button element with different image when clicked
-
-
-//Buttons setup BEGIN
-
-//LEFT BUTTON
-leftButton = createButtonWithImage("left.png", "left-click.png", 50, 50, 0, 40);
-
-leftButton.onclick = function(){
-  leftPosition = (leftPosition >= -IMAGE_WIDTH)? minLeft:(leftPosition - (leftPosition % IMAGE_WIDTH)) + IMAGE_WIDTH; 
-  updateWrapperPosition(leftPosition);
-}
-container.appendChild(leftButton);
-
-//RIGHT BUTTON
-rightButton = createButtonWithImage(
-  "right.png",
-  "right-click.png",
-  50,
-  50,
-  95,
-  40
-);
-
-rightButton.onclick = function() {
-  leftPosition = (leftPosition <= (minLeft-IMAGE_WIDTH))? 0: (leftPosition - (leftPosition % IMAGE_WIDTH)) - IMAGE_WIDTH; 
-  console.log(leftPosition);
-  updateWrapperPosition(leftPosition);
-};
-container.appendChild(rightButton);
-
-
-//INDICATOR BUTTONS
-for (i = 0; i < wrapper.childElementCount; i++) {
-  indicatorButtons.push(
-    createButtonWithImage(
-      "icon-circle.png",
-      "icon-circle-click.png",
-      20,
-      20,
-      50 + i * 2,
-      90
-    )
-  ); 
-}
-
-indicatorButtons[0].onclick = function(){
-  leftPosition = 0 * (-IMAGE_WIDTH);
-  updateWrapperPosition(leftPosition);
-}
-container.appendChild(indicatorButtons[0]);
-
-indicatorButtons[1].onclick = function(){
-  leftPosition = 1 * (-IMAGE_WIDTH);
-  updateWrapperPosition(leftPosition);
-}
-container.appendChild(indicatorButtons[1]);
-
-indicatorButtons[2].onclick = function(){
-  leftPosition = 2 * (-IMAGE_WIDTH);
-  updateWrapperPosition(leftPosition);
-}
-container.appendChild(indicatorButtons[2]);
-
-
-//Buttons setup END
-
-function createButtonWithImage(
-  imageName,
-  clickImageName,
-  width,
-  height,
-  leftPositionInPercent,
-  topPositionInPercent
-) {
-  var button = document.createElement("button");
-  button.style.backgroundColor = "transparent";
-  button.style.border = "none";
-  button.style.width = width + "px";
-  button.style.height = height + "px";
-  button.onfocus = function() {
-    button.style.outline = "0";
-  };
-  var image = document.createElement("img");
-  image.setAttribute("src", "images/" + imageName);
-  image.style.width = "100%";
-  button.appendChild(image);
-
-  button.style.position = "absolute";
-  button.style.left = leftPositionInPercent + "%";
-  button.style.top = topPositionInPercent + "%";
-
-  button.onmouseleave = function() {
-    button.childNodes[0].setAttribute("src", "images/" + imageName);
-  };
-  button.onmouseenter = function() {
-    button.childNodes[0].setAttribute("src", "images/" + clickImageName);
-  };
-
-  
-
-
-  return button;
-}
-
-startTransition();
-
+let canvasElement = document.getElementById("canvas");
+let canvas = new Canvas(canvasElement, 500, 500);
+canvas.start();
